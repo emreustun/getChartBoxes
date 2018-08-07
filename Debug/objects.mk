2268e7c755781d789d8f75a8df62d3da255b6526
emreUstunOgam <i.emre.ustun@gmail.com>
2018-08-07 14:09:03 +0300

    Unnecessary files have been removed.

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lopencv_imgcodecs -lopencv_imgproc -lopencv_highgui -lopencv_core
-
