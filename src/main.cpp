65211bc501953876bb62639b6685b7fa3070f9de
emreUstunOgam <i.emre.ustun@gmail.com>
2018-08-13 13:19:13 +0300

    Help added. Some Unnecessary comments removed.

--- src/main.cpp
+++ src/main.cpp
@@ -17,10 +17,7 @@
 #include <stdlib.h>
 #include <typeinfo>
 
-
-
 cv::Mat img0, img1, res1, final, tmp, dst;
-
 cv::Point point;
 int drag = 0;
 
@@ -45,20 +42,11 @@ void mouseHandler(int event, int x, int y, int, void*) // (x,y) is where the mou
                 img1 = img0.clone();
             point = cv::Point(x, y); //
             cv::circle(img1,point,2,cv::Scalar(0, 0, 255),-1, 8, 0);
-            printf("BUTTON DOWN!! x = %d, y = %d\n", x, y);
+//            printf("BUTTON DOWN!! x = %d, y = %d\n", x, y);
             pts[var] = point;
             var++;
             drag  = 1;
             if(var>1)
-//                cv::line(img1,pts[var-2], point, cv::Scalar(0, 255, 0), 2, 8, 0);
-//                cv::rectangle(img1, pts[var-2], pts[var-1], cv::Scalar(0, 255, 0), 2, 8, 0);
-//            printf("[VAR-2]%d, x = %d, y = %d\n", var-1, pts[var-2].x, pts[var-2].y);
-//            printf("[VAR-1]%d, x = %d, y = %d\n", var-2, pts[var-1].x, pts[var-1].y);
-//            for (int i=0; i<=var; i++)
-//            {
-//                printf("%d, x = %d, y = %d\n", i, pts[i].x, pts[i].y);
-//            }
-
             imshow("Source", img1);
         }
     }
@@ -66,7 +54,6 @@ void mouseHandler(int event, int x, int y, int, void*) // (x,y) is where the mou
     if (event == cv::EVENT_LBUTTONUP && drag)
     {
         imshow("Source", img1);
-//        printf("BUTTON UP!! x = %d, y = %d\n", x, y);
         drag = 0;
     }
     if (event == cv::EVENT_RBUTTONDOWN)
@@ -78,8 +65,6 @@ void mouseHandler(int event, int x, int y, int, void*) // (x,y) is where the mou
 
         if(var!=0)
         {
-            const cv::Point* pts3[1] = {&pts[0]};
-//            cv::polylines( img1, pts3, &numpts,1, 1, cv::Scalar(255, 0, 0), 2, 8, 0);
             cv::rectangle(img1, pts[var-2], pts[var-1], cv::Scalar(0, 255, 0), 2, 8, 0);
         }
 
@@ -105,11 +90,10 @@ void mouseHandler(int event, int x, int y, int, void*) // (x,y) is where the mou
         pts[2] = cv::Point(maxx, maxy);
         pts[3] = cv::Point(maxx, miny);
         const cv::Point* pts4[1] = {&pts[0]};
-        printf("pts4,0 ==> %d, %d\n", pts4[0]->x, pts4[0]->y);
-        printf("pts4,1 ==> %d, %d\n", (pts4[0]+1)->x, (pts4[0]+1)->y);
-        printf("pts4,2 ==> %d, %d\n", (pts4[0]+2)->x, (pts4[0]+2)->y);
-        printf("pts4,3 ==> %d, %d\n", (pts4[0]+3)->x, (pts4[0]+3)->y);
-        printf("pts4,4 ==> %d, %d\n", (pts4[0]+4)->x, (pts4[0]+4)->y);
+        printf("Corner 1 ==> %d, %d\n", pts4[0]->x, pts4[0]->y);
+        printf("Corner 2 ==> %d, %d\n", (pts4[0]+1)->x, (pts4[0]+1)->y);
+        printf("Corner 3 ==> %d, %d\n", (pts4[0]+2)->x, (pts4[0]+2)->y);
+        printf("Corner 4==> %d, %d\n", (pts4[0]+3)->x, (pts4[0]+3)->y);
 
         cv::fillPoly(res1, pts4,&numpts, 1, cv::Scalar(177), 8, 0);
         bitwise_and(img0, img0, final,res1); // img0 & img0 = final, masked by res1
@@ -137,26 +121,35 @@ void mouseHandler(int event, int x, int y, int, void*) // (x,y) is where the mou
     {
         if (event > 0)
         {
-            printf("scrolling Up!!!!\n");
+//            printf("scrolling Up!!!!\n");
         }
     }
 
     if (event == cv::EVENT_LBUTTONDBLCLK)
     {
-        printf("scrolling Down!!!!\n");
+//        printf("scrolling Down!!!!\n");
     }
+}
+
+void help()
+{
+    std::cout << "Instructions:" << std::endl;
+    std::cout << "Left button selects the corners of the rectangle"  << std::endl;
+    std::cout << "Middle button resets the process" << std::endl;
+    std::cout << "Right button extracts the mask and saves it" << std::endl << std::endl;
 }
 
 int main(int argc, char **argv)
 {
+    help();
+
     //Insert image path
-    cv::Mat src = cv::imread("tempImage.jpeg");
+    cv::Mat src = cv::imread("tempImage.jpg");
     minx = INT_MAX; miny = INT_MAX; maxx = INT_MIN; maxy = INT_MIN;
     img0 = src;
 
     res1 = cv::Mat::zeros(img0.size(),CV_8UC1);
     final = cv::Mat::zeros(img0.size(),CV_8UC3);
-    //////////// source image ///////////////////
 
     cv::namedWindow("Source", 1);
     cv::setMouseCallback("Source", mouseHandler, NULL);
